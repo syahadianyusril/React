ebde426ad5f68813ecad7dd0c31fb1ce3536309f
syahadianyusril <syahadianyusril@gmail.com>
2019-03-11 16:09:25 +0700

    blog - center

--- challange_react/src/component/Search.js
+++ challange_react/src/component/Search.js
@@ -3,19 +3,20 @@ import React, { Component } from 'react';
 class Search extends Component {
   render() {
     return (
-      <nav class="navbar navbar-light bg-light justify-content-between">
-        <a class="navbar-item" href="#">Sports</a>
-        <a class="navbar-item" href="#">Lifestyle</a>
-        <a class="navbar-item" href="#">Health</a>
-        <a class="navbar-item" href="#">Entertainment</a>
-        <a class="navbar-item" href="#">Comic</a>
-        <a class="navbar-item" href="#">Art</a>
-
-        <form class="form-inline">
-          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </nav>
+      <div class="container">
+        <nav class="navbar navbar-light bg-transparent">
+          <a class="navbar-item" href="#">Sports</a>
+          <a class="navbar-item" href="#">Lifestyle</a>
+          <a class="navbar-item" href="#">Health</a>
+          <a class="navbar-item" href="#">Entertainment</a>
+          <a class="navbar-item" href="#">Comic</a>
+          <a class="navbar-item" href="#">Art</a>
+          <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </nav>
+      </div>  
     )
   }
 }
