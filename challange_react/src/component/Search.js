e598b078a6d2e4577f0ef792c0c574013a575799
syahadianyusril <syahadianyusril@gmail.com>
2019-03-11 16:22:33 +0700

    search biru

--- challange_react/src/component/Search.js
+++ challange_react/src/component/Search.js
@@ -13,7 +13,7 @@ class Search extends Component {
           <a class="navbar-item" href="#">Art</a>
           <form class="form-inline">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
           </form>
         </nav>
       </div>  
