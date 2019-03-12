eb640b942bd73b3e5df7b4efbd6aee406afdfefa
syahadianyusril <syahadianyusril@gmail.com>
2019-03-12 14:43:22 +0700

    pagi

--- challange_react/src/AppBlog.js
+++ challange_react/src/AppBlog.js
@@ -4,18 +4,70 @@ import Footer from './component/Footer.js'
 import Search from './component/Search.js'
 import Blogcontent from './blog/Blogcontent.js'
 import Blogmenu from './blog/Blogmenu.js'
+import axios from 'axios'
 
+const imgNull = 0;
+
+const baseUrl = "https://newsapi.org/v2/";
+const keyApi = "79ea232ad60645a8a122c07c03321932";
+const search = "google"
+const country = "us"
+
+// endpoint semua berita
+const urlHeadline = baseUrl + "everything?q=" + search + "&apiKey=" + keyApi ;
+// endopoint headlines
+const urlHeadlineNew = baseUrl + "top-headlines?country=" + country + "&apiKey=" + keyApi ;
 
 class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      listNews : [],
+      listNewsNew : [],
+      userName : "",
+      isLogin: false
+    }
+  }
+
+  componentDidMount = () => {
+    const self = this;
+    axios.all([
+      axios.get(urlHeadline),
+      axios.get(urlHeadlineNew) 
+    ])
+    .then(axios.spread(function(response1,response2){
+      self.setState({listNews : response1.data.articles})
+      self.setState({listNewsNew : response2.data.articles})
+    }))
+    .catch(function(error){
+    });
+  }
+
   render() {
+    const {listNews, listNewsNew,username, isLogin } = this.state;
+
     return (
       <div>
         <Header />
         <Search />
         <div className="container">
             <div class="row">
-                <Blogcontent />
-                <Blogmenu />
+                <div className="col-md-4 my-4">
+                  <h5 className="card-header bg-primary" style={{ color: "white" }}>
+                    Berita Terkini
+                  </h5>
+                  {listNewsNew.map((item, key) => {
+                    return <Blogmenu key={key} title={item.title} url={item.url}/>;
+                  })}
+                </div>
+                <div className="col-md-8">
+                  {listNews.map((item, key) => {
+                    const src_img = item.urlToImage === null ? imgNull : item.urlToImage;
+                    const src_content = item.urlToImage !== null ? item.content : "";
+
+                    return <Blogcontent key={key} title={item.title} img={src_img} content={src_content}  author={item.author} publishedAt={item.publishedAt}/>;
+                  })}
+                </div>
             </div>
         </div>
         <Footer />
--- challange_react/src/blog/Blogcontent.js
+++ challange_react/src/blog/Blogcontent.js
@@ -1,27 +1,55 @@
 import React, { Component } from 'react';
+import PropTypes from "prop-types";
 
-class Blogcontent extends Component {
-  render() {
-    return (
-      <div className="col-md-8">
+// class Blogcontent extends Component {
+//   render() {
+//     return (
+//       <div className="col-md-8">
 
-        <h1 className="my-4">Category - Sports</h1>
+//         <h1 className="my-4">Category - Sports</h1>
 
-        <div className="card mb-4">
-          <img className="card-img-top" src={require("../assets/img/blogpost.jpg")} alt="Card image cap"/>
-          <div className="card-body">
-            <h2 className="card-title">Pemain american football mati mengenaskan ketika mendengar suara mantan</h2>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-            <a href="#" className="btn btn-primary">Read More &rarr;</a>
-          </div>
-          <div className="card-footer text-muted">
-            Posted on January 1, 2019 by
-            <a href="#"> Saya sendiri</a>
-          </div>
+//         <div className="card mb-4">
+//           <img className="card-img-top" src={require("../assets/img/blogpost.jpg")} alt="Card image cap"/>
+//           <div className="card-body">
+//             <h2 className="card-title">Pemain american football mati mengenaskan ketika mendengar suara mantan</h2>
+//             <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+//             <a href="#" className="btn btn-primary">Read More &rarr;</a>
+//           </div>
+//           <div className="card-footer text-muted">
+//             Posted on January 1, 2019 by
+//             <a href="#"> Saya sendiri</a>
+//           </div>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+const Blogcontent = props => {
+  return (
+    <div>
+      <div className="card my-4">
+        <img className="card-img-top" src={props.img} alt="Card image cap"/>
+        <div className="card-body">
+          <h2 className="card-title">{props.title}</h2>
+          <p className="card-text">{props.content}</p>
+          <a href="#" className="btn btn-primary">Read More &rarr;</a>
+        </div>
+        <div className="card-footer text-muted">
+          Posted At: {props.publishedAt}<br></br>
+          Pested By: <a>{props.author}</a>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+Blogcontent.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  publishedAt: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired
 }
 
 export default Blogcontent;
